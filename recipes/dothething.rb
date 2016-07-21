fc0c1bb482d13f65b5b111c1169ce7f980fea104
Jeremy J. Miller <jm@chef.io>
2016-07-21 16:55:15 -0400

    update dothething.rb

--- recipes/dothething.rb
+++ recipes/dothething.rb
@@ -1,9 +1,10 @@
 #
 # Cookbook Name:: test123
-# Recipe:: default
+# Recipe:: dothething
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+# we need to create a file owned by the ldap user
 file '/tmp/foo' do
   owner node['ldapuser']
   content 'hello world'
